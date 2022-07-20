35352eb32a5a3b59c5bc78557e29966de9b0b9bb
“BillMuriu” <njorogekamau63@gmail.com>
2022-07-20 19:33:31 +0100

    updated project

---
+++ src/src/container/Intro/Intro.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
+import { meal } from '../../constants';
+import './Intro.css';
+
+import './Intro.css';
+
+const Intro = () => {
+  <div className="app__video">
+    <video/>
+  </div>
+};
+
+export default Intro;
